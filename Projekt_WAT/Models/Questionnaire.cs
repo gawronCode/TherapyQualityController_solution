03423b74071fde61a7a2ca05b9cd0a932ac66879
gawronCode <gawronCode@gmail.com>
2021-01-11 21:33:09 +0100

    Add project files.

---
+++ Projekt_WAT/Models/Questionnaire.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TherapyQualityController.Models
+{
+    public class Questionnaire
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+    }
+}
---
+++ Projekt_WAT/Program.cs
@@ -0,0 +1,24 @@
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
+
+// Aplikacja do monitorowania przyjmowania leków i wyœwietlania skutecznoœci 
+    // dzia³ania terapii w kontekœcie wype³nianych 5 razy dziennie ankiet pacjenta o 
+    // symptomach rejestrowanych w postaci pytañ
+
+namespace TherapyQualityController
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
+}
