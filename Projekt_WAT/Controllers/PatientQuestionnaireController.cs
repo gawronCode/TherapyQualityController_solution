4034cef6237f47cdb131ff34d71d24836900c132
gawronCode <gawronCode@gmail.com>
2021-01-12 22:56:11 +0100

    Add PatientQuestionnaire View

--- Projekt_WAT/Controllers/PatientQuestionnaireController.cs
+++ Projekt_WAT/Controllers/PatientQuestionnaireController.cs
@@ -3,16 +3,59 @@ using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using TherapyQualityController.Models;
+using TherapyQualityController.Repositories;
 
 namespace TherapyQualityController.Controllers
 {
     public class PatientQuestionnaireController : Controller
     {
+
+        private readonly IQuestionnaireRepo _questionnaireRepo;
+        private readonly IQuestionRepo _questionRepo;
+        private readonly UserManager<User> _userManager;
+
+        public PatientQuestionnaireController(IQuestionnaireRepo questionnaireRepo,
+            IQuestionRepo questionRepo)
+        {
+            _questionnaireRepo = questionnaireRepo;
+            _questionRepo = questionRepo;
+
+        }
+
         // GET: PatientQuestionnaireController
         public ActionResult Index()
         {
-            return View();
+            // var userEmail = User.FindFirstValue(ClaimTypes.Email);
+            // var user = _userManager.FindByEmailAsync(userEmail).Result;
+            // var userQuestionnaireId = user.QuestionnaireId;
+
+            var questionnaire = _questionnaireRepo.GetById(2).Result;
+            var questions = _questionRepo.GetQuestionsByQuestionnaireId(2).Result;
+
+            var model = new QuestionnaireViewModel
+            {
+                Name = questionnaire.Name,
+                Fields = new List<FieldViewModel>()
+            };
+
+            foreach (var question in questions)
+            {
+                var questionVm = new QuestionViewModel
+                {
+                    Contents = question.Contents
+                };
+                model.Fields.Add(new FieldViewModel
+                {
+                    Answer = new AnswerViewModel(),
+                    Question = questionVm
+                });
+            }
+
+            return View(model);
         }
 
         // GET: PatientQuestionnaireController/Details/5
