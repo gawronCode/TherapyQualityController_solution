1ff530088e41a4ee360f69f8fc4a81372a9ce1e0
gawronCode <gawronCode@gmail.com>
2021-01-22 21:59:40 +0100

    UserPersmissionController is now async

--- Projekt_WAT/Controllers/UserPermissionsController.cs
+++ Projekt_WAT/Controllers/UserPermissionsController.cs
@@ -1,8 +1,5 @@
-﻿using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
-using System;
+﻿using Microsoft.AspNetCore.Mvc;
 using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
@@ -16,38 +13,26 @@ namespace TherapyQualityController.Controllers
     public class UserPermissionsController : Controller
     {
 
-        private readonly IQuestionnaireRepo _questionnaireRepo;
-        private readonly IQuestionRepo _questionRepo;
         private readonly IUserRepo _userRepo;
-        private readonly IAnswerRepo _answerRepo;
-        private readonly IPatientQuestionnaireRepo _patientQuestionnaireRepo;
         private readonly UserManager<User> _userManager;
 
-        public UserPermissionsController(IQuestionnaireRepo questionnaireRepo,
-            IQuestionRepo questionRepo,
-            IUserRepo userRepo,
-            IAnswerRepo answerRepo,
-            IPatientQuestionnaireRepo patientQuestionnaireRepo,
+        public UserPermissionsController(IUserRepo userRepo,
             UserManager<User> userManager)
         {
-            _questionnaireRepo = questionnaireRepo;
-            _questionRepo = questionRepo;
             _userRepo = userRepo;
-            _answerRepo = answerRepo;
-            _patientQuestionnaireRepo = patientQuestionnaireRepo;
             _userManager = userManager;
         }
 
-        // GET: UserPermissionsController
-        public ActionResult Index()
+
+        public async Task<ActionResult> Index()
         {
-            var patients = _userRepo.GetAll().Result;
+            var patients = await _userRepo.GetAll();
             var model = new List<DoctorViewModel>();
 
             foreach (var patient in patients)
             {
                 if (string.IsNullOrEmpty(patient.PWZ)) continue;
-                if (_userManager.IsInRoleAsync(patient, "Doctor").Result)
+                if (await _userManager.IsInRoleAsync(patient, "Doctor"))
                 {
                     model.Add(new DoctorViewModel
                     {
@@ -76,20 +61,20 @@ namespace TherapyQualityController.Controllers
             return View(model);
         }
 
-        public ActionResult AddPermissions(string id)
+        public async Task<ActionResult> AddPermissions(string id)
         {
-            var user = _userManager.FindByEmailAsync(id).Result;
-            _userManager.RemoveFromRoleAsync(user, "Patient").Wait();
-            _userManager.AddToRoleAsync(user, "Doctor").Wait();
+            var user = await _userManager.FindByEmailAsync(id);
+            await _userManager.RemoveFromRoleAsync(user, "Patient");
+            await _userManager.AddToRoleAsync(user, "Doctor");
 
             return RedirectToAction(nameof(Index));
         }
 
-        public ActionResult RemovePermissions(string id)
+        public async Task<ActionResult> RemovePermissions(string id)
         {
-            var user = _userManager.FindByEmailAsync(id).Result;
-            _userManager.RemoveFromRoleAsync(user, "Doctor").Wait();
-            _userManager.AddToRoleAsync(user, "Patient").Wait();
+            var user = await _userManager.FindByEmailAsync(id);
+            await _userManager.RemoveFromRoleAsync(user, "Doctor");
+            await _userManager.AddToRoleAsync(user, "Patient");
 
             return RedirectToAction(nameof(Index));
         }
